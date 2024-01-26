ff2b7151d0ad07acc51205d48513aee22c489927
Pedro Luiz Forlan de Paula <pedro.forlan2000@gmail.com>
2024-01-26 15:44:38 -0300

    first commit

---
+++ src/main/java/com/pedroluizforlan/rectiveflashcards/RectiveFlashcardsApplication.java
@@ -0,0 +1,13 @@
+package com.pedroluizforlan.rectiveflashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RectiveFlashcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RectiveFlashcardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroluizforlan/rectiveflashcards/RectiveFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroluizforlan.rectiveflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RectiveFlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
